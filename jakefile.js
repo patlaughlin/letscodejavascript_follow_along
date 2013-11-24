c981aba59a540d3d525a61c3d56902024e8ceb3a
patlaughlin <plaughlin@skookum.com>
2013-11-24 13:28:50 -0500

    basic test runner working

---
+++ jakefile.js
@@ -0,0 +1,60 @@
+// Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
+
+/*global desc, task, jake, fail, complete */
+(function() {
+    "use strict";
+
+    desc("Build and test");
+    task("default", ["test"]);
+
+    desc("Lint everything");
+    task("lint", [], function() {
+        var lint = require("./build/lint/lint_runner.js");
+
+        var files = new jake.FileList();
+        files.include("**/*.js");
+        files.exclude("node_modules");
+        var options = nodeLintOptions();
+        var passed = lint.validateFileList(files.toArray(), options, {});
+        if (!passed) fail("Lint failed");
+    });
+
+    desc("Test everything");
+    task("test", [], function() {
+        var reporter = require("nodeunit").reporters["default"];
+        reporter.run(['src/server/_server_test.js']);
+    });
+
+    desc("Integrate");
+    task("integrate", ["default"], function() {
+        console.log("1. Make sure 'git status' is clean.");
+        console.log("2. Build on the integration box.");
+        console.log("   a. Walk over to integration box.");
+        console.log("   b. 'git pull'");
+        console.log("   c. 'jake'");
+        console.log("   d. If jake fails, stop! Try again after fixing the issue.");
+        console.log("3. 'git checkout integration'");
+        console.log("4. 'git merge master --no-ff --log'");
+        console.log("5. 'git checkout master'");
+    });
+
+    function nodeLintOptions() {
+        return {
+            bitwise:true,
+            curly:false,
+            eqeqeq:true,
+            forin:true,
+            immed:true,
+            latedef:true,
+            newcap:true,
+            noarg:true,
+            noempty:true,
+            nonew:true,
+            regexp:true,
+            undef:true,
+            strict:true,
+            trailing:true,
+            node:true
+        };
+    }
+}());
